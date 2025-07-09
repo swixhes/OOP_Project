fd20adf58105771655fe598583506881a274f327
Alona Chuchupalova <a.s.chuchupalova@student.khai.edu>
2025-07-09 08:53:31 +0300

    SCRUM-28: Groupon Platform Analysis

--- DiscountMarketplace.Domain/Coupon.cs
+++ DiscountMarketplace.Domain/Coupon.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//SCRUM-28: Groupon Platform Analysis
 namespace DiscountMarketplace.Domain
 {
     public class Coupon
